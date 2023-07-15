6647632aafe4f507f17729c9240fcf8a2e98d871
umbrelluck <yarko.umbrelluck@gmail.com>
2023-07-15 19:49:57 +0300

    added suda and working autopairs

--- nvim/.config/nvim/lua/plugins/autocmp/_nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autocmp/_nvim-autopairs.lua
@@ -1,21 +1,25 @@
+local opts = {
+    enable_check_bracket_line = false,
+    ignored_next_char = "[%w%.]",
+    fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        manual_position = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
+    },
+}
+
 return { {
     "windwp/nvim-autopairs",
-    opts = {
-        enable_check_bracket_line = false,
-        ignored_next_char = "[%w%.]",
-        fast_wrap = {
-            map = "<M-e>",
-            chars = { "{", "[", "(", '"', "'" },
-            pattern = [=[[%'%"%>%]%)%}%,]]=],
-            end_key = "$",
-            keys = "qwertyuiopzxcvbnmasdfghjkl",
-            check_comma = true,
-            manual_position = true,
-            highlight = "Search",
-            highlight_grey = "Comment",
-        },
-    },
+    event = "InsertEnter",
     config = function()
+        require("nvim-autopairs").setup(opts)
+
         require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
     end,
 } }
