9b6512bbcb3363bae847110ee121b9501214e05b
umbrelluck <yarko.umbrelluck@gmail.com>
2023-07-21 22:02:34 +0300

    init.lua removed commented lines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,29 +1,4 @@
 require("config.globals")
-
--- require("plugins_c")
 require("config.remaps")
 require("config.set")
 require("config.lazy")
-
--- require("_treesitter")
---
--- require("_notify")
--- -- require('noice_c')
---
--- require("_neogit")
--- require("_gitsigns")
--- require("_diffview")
---
--- require("_lspconfig")
--- require("_nvim-cmp")
--- require("_null-ls")
--- require("_comment")
---
--- require("_trouble")
--- require("_telescope")
--- require("_harpoon")
--- require("_undotree")
--- require("_indent-blankline")
---
--- require("_lualine")
--- require("_onedark")
