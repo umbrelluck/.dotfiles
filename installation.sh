6429337f11fc82e8a7561aa8255d52784fe0c235
umbrelluck <yarko.umbrelluck@gmail.com>
2023-07-25 19:29:37 +0300

    moved nested folders for stow

--- installation.sh
+++ installation.sh
@@ -1,3 +1,31 @@
 #! /bin/bash
 
-echo "This file is in progress"
+confd="$XDG_CONFIG_HOME"
+homed="$HOME"
+
+mkdir -p "$confd/alacritty"
+stow -t "$confd/alacritty" alacritty 
+
+mkdir -p "$confd/nvim"
+stow -t "$confd/nvim" nvim 
+
+mkdir -p "$confd/paru"
+stow -t "$confd/paru" paru 
+
+stow -t "$homed" powerlevel10k 
+
+mkdir -p "$confd/qtile"
+stow -t "$confd/qtile" qtile 
+
+mkdir -p "$confd/alacritty"
+stow -t "$confd/ranger" ranger 
+
+stow -t "$homed" scripts 
+
+mkdir -p "$confd/X11"
+stow -t "$confd/X11" X11 
+
+mkdir -p "$confd/xplr"
+stow -t "$confd/xplr" xplr 
+
+stow -t "$homed" zsh 
