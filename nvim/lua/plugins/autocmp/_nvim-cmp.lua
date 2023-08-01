7ae0aa14938c09e062f3422b8a3036a259b55f10
umbrelluck <yarko.umbrelluck@gmail.com>
2023-08-01 22:13:09 +0300

    nvim cmp event on cmd

--- nvim/lua/plugins/autocmp/_nvim-cmp.lua
+++ nvim/lua/plugins/autocmp/_nvim-cmp.lua
@@ -1,7 +1,7 @@
 return {
     {
         "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+        event = { "InsertEnter", "CmdlineEnter" },
         dependencies = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
