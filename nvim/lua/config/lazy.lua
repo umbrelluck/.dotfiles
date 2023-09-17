35f7ac512bcc2629a322344a65bb83755eb82635
umbrelluck <yarko.umbrelluck@gmail.com>
2023-09-17 13:21:02 +0300

    cmake and new theme

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 local specs = {
     { import = "plugins.lsp" },
     { import = "plugins.autocmp" },
+    { import = "plugins.build" },
     { import = "plugins.highlighting" },
     { import = "plugins.git" },
     { import = "plugins.debug" },
---
+++ nvim/lua/plugins/themes/_caret.lua
@@ -0,0 +1,34 @@
+return {
+    'projekt0n/caret.nvim',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    enabled = false,
+    opts = {
+        options = {
+            transparent = false,    -- Set to true to disable background setting
+            inverted_signs = false, -- Controls inverted Signcolumn highlighting
+            styles = {              -- Define styles for various syntax groups
+                bold = true,
+                italic = true,
+                strikethrough = true,
+                undercurl = true,
+                underline = true,
+            },
+            inverse = { -- Determines inverse highlights for different types
+                match_paren = false,
+                visual = false,
+                search = false,
+            },
+        },
+        mapping = {          -- Configure key mappings
+            toggle_bg = nil, -- Assign a specific key for toggling background
+        },
+        groups = {},         -- Override default highlight groups here
+    },
+    config = function(_, opts)
+        require('caret').setup(opts)
+
+        vim.opt.background = 'dark'
+        vim.cmd('colorscheme caret')
+    end,
+}
