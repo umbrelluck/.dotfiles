3023fd21a7c3fae38ef363b708671cf2ef2c3e2a
umbrelluck <yarko.umbrelluck@gmail.com>
2023-12-25 21:00:37 +0200

    lemurs

--- installation.sh
+++ installation.sh
@@ -2,8 +2,9 @@
 
 confd="$XDG_CONFIG_HOME"
 homed="$HOME"
+etcd="/etc"
 
-mkdir -p "$confd/alacritty/a_themes"
+mkdir -p "$confd/alacritty/a_themes" 
 stow -t "$confd/alacritty" alacritty 
 git clone https://github.com/alacritty/alacritty-theme "$confd/alacritty/a_themes"
 
@@ -25,8 +26,13 @@ stow -t "$confd/X11" X11
 mkdir -p "$confd/xplr"
 stow -t "$confd/xplr" xplr 
 
+echo "Lemurs setup need password to write files"
+sudo ln -s "$homed/.dotfiles/lemurs/wms" "$etcd/lemurs/wms"
+sudo ln -s "$homed/.dotfiles/lemurs/wayland" "$etcd/lemurs/wayland"
+
 stow -t "$homed" scripts 
 stow -t "$homed" zsh 
 zsh && . "$SCRSDIR/zsh_plugin&script_downloader.sh"
 stow -t "$homed" powerlevel10k 
 stow -t "$homed" tmux
+
