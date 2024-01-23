aa72e6bc7b045804e5c6b3955260cda6eaa8bd54
umbrelluck <yarko.umbrelluck@gmail.com>
2024-01-24 01:43:32 +0200

    obsidian and search for links

--- nvim/lua/plugins/motions/_hop.lua
+++ nvim/lua/plugins/motions/_hop.lua
@@ -5,7 +5,8 @@ return { {
         keys = "etovxqpdygfblzhckisuran",
         current_line_only = false,
     },
-    keys = { "f", "F", "t", "T", "<Leader>hw", "<Leader>he" },
+    cmd = { "HopPatternCurrentLine" },
+    keys = { "f", "F", "t", "T", "<Leader>hw", "<Leader>he", "gl", "gL" },
     config = function(_, opts)
         local hop = require("hop")
         local directions = require("hop.hint").HintDirection
@@ -17,13 +18,21 @@ return { {
         end, { remap = true, desc = "Remap for f" })
         _G.emap("F", function()
             hop.hint_char1({ direction = directions.BEFORE_CURSOR, })
-        end, { remap = true, desc = "REmap for F" })
+        end, { remap = true, desc = "Remap for F" })
         _G.emap("t", function()
             hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
         end, { remap = true, desc = 'Remap for t' })
         _G.emap("T", function()
             hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
         end, { remap = true, desc = "Remap for T" })
+
+        _G.nmap("gl", function()
+            hop.hint_patterns({ direction = directions.AFTER_CURSOR, current_line_only = true, }, "http")
+        end, { desc = "Find nearest url after cursor" })
+
+        _G.nmap("gL", function()
+            hop.hint_patterns({ direction = directions.AFTER_CURSOR, current_line_only = true, }, "http")
+        end, { desc = "Find nearest url before cursor" })
 
         _G.nmap("<Leader>hw", ":HopWord<cr>", { desc = "Hop word" })
         _G.nmap("<Leader>he", ":HopPattern<cr>", { desc = "Hop pattern" })
--- nvim/lua/plugins/organization/_obsidian.lua
+++ nvim/lua/plugins/organization/_obsidian.lua
@@ -1,39 +1,49 @@
 return {
     "epwalsh/obsidian.nvim",
-    enabled = false,
-    event = "BufReadPre */obsidian/*.md",
+    -- enabled = false,
+    lazy = true,
+    ft = "markdown",
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
+    event = {
+        "BufReadPre " .. vim.fn.expand("~") .. "/Documents/Vault/**.md",
+        "BufNewFile " .. vim.fn.expand("~") .. "/Documents/Vault/**.md",
+    },
     opts = {
-        dir = "~/obsidian/Vaultum/",
-        daily_notes = {
-            folder = "daily",
+        workspaces = {
+            {
+                name = "Courses",
+                path = "~/Vault/Courses",
+            },
         },
         completion = {
             nvim_cmp = true,
             min_chars = 2,
-            -- "current_dir", "notes_subdir"
-            new_notes_location = "current_dir",
-            prepend_note_id = false
+            new_notes_location = "current_dir", -- current_dir | notes_subdir
+
+            -- Control how wiki links are completed with these (mutually exclusive) options:
+            -- 1. Whether to add the note ID during completion.
+            -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
+            prepend_note_id = false,
+            -- 2. Whether to add the note path during completion.
+            -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
+            prepend_note_path = true,
+            -- 3. Whether to only use paths during completion.
+            -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
+            use_path_only = false,
         },
-        -- Accepted values are "current", "hsplit" and "vsplit"
-        open_notes_in = "current"
+
+        open_notes_in = "current" -- curent | hsplit | vsplit
     },
     config = function(_, opts)
         require("obsidian").setup(opts)
 
-        _G.nmap("gf", function()
-            if require("obsidian").util.cursor_on_markdown_link() then
-                return "<cmd>ObsidianFollowLink<CR>"
-            else
-                return "gf"
-            end
-        end, { noremap = false, expr = true })
+        _G.nmap("<leader>oo", ":ObsidianOpen ", { silent = false, desc = "[Obsidian]: [o]pen file in Obsidian" })
+        _G.nmap("<leader>on", ":ObsidianNew ", { silent = false, desc = "[O]bsidian: create new [n]ote" })
+        _G.nmap("<leader>ow", ":ObsidianQuickSwitch ", { silent = false, desc = "[Obsidian]: quick s[w]itch note" })
+        _G.nmap("<leader>of", ":ObsidianFollowLink ", { silent = false, desc = "[Obsidian]: [f]ollow link" })
 
-        -- _G.nmap("<leader>oo", ":ObsidianOpen ", { silent = false, desc = "[Obsidian]: [o]pen file in Obsidian" })
-        -- _G.nmap("<leader>on", ":ObsidianNew ", { silent = false, desc = "[O]bsidian: create new [n]ote" })
-        -- _G.nmap("<leader>ow", ":ObsidianQuickSwitch ", { silent = false, desc = "[Obsidian]: quick s[w]itch note" })
-        -- _G.nmap("<leader>of", ":ObsidianFollowLink ", { silent = false, desc = "[Obsidian]: [f]ollow link" })
+        vim.o.conceallevel = 1
     end
 }
