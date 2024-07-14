a8438f1f23cbb6eae031e6ee4de38804e467d1fe
umbrelluck <yarko.umbrelluck@gmail.com>
2024-07-14 21:55:07 +0300

    Bluetooth script

--- scripts/bluetooth/bluetuith.sh
+++ scripts/bluetooth/bluetuith.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-# alacritty  & 
-#
+alacritty --class 'bluetuith' -e 'bluetuith'
+
 # BLUETUITH_ID=$!
-#
+
 # sleep 0.105
 # hyprctl dispatch setfloating pid:"$BLUETUITH_ID"
 # sleep 0.105
@@ -12,7 +12,7 @@
 # hypcrtl dispatch resizewindowpixel exact 20% 10% pid:"$BLUETUITH_ID"
 # sleep 0.105
 # # hyprctl dispatch centerwindow
-#
-#
 
-alacritty -e " sleep 0.105 && hyprctl dispatch setfloating active && sleep 0.105 && hypcrtl dispatch resizeactive exact 20% 10% && sleep 0.105 && hyprctl dispatch centerwindow"
+
+
+# alacritty -e " sleep 0.105 && hyprctl dispatch setfloating active && sleep 0.105 && hypcrtl dispatch resizeactive exact 20% 10% && sleep 0.105 && hyprctl dispatch centerwindow"
