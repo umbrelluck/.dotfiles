7e4b760cde78000cf6cd4bfab699f720a0629480
umbrelluck <yarko.umbrelluck@gmail.com>
2024-11-27 20:19:55 +0200

    Dunst
    Config and notifications with cronie

---
+++ scripts/battery/battery_notify.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/zsh
+
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
+WARNING_LEVEL=20
+CRITICAL_LEVEL=9
+
+BATTERY_DISCHARGING=$(acpi -b | grep "Battery 0" | grep -c "Discharging")
+BATTERY_LEVEL=$(acpi -b | grep "Battery 0" | grep -Po '[0-9]+(?=%)')
+
+FULL_FILE=/tmp/batteryfull
+WARNING_FILE=/tmp/batterywarn
+CRITICAL_FILE=/tmp/batterycritical
+
+# Reset notifications if the computer is charging/discharging
+if [ "$BATTERY_DISCHARGING" -eq 1 ] && [ -f $FULL_FILE ]; then
+	rm $FULL_FILE
+elif [ "$BATTERY_DISCHARGING" -eq 0 ] && [ -f $WARNING_FILE ]; then
+	rm $WARNING_FILE
+    [ -f $CRITICAL_FILE ] && rm $CRITICAL_FILE
+fi
+
+# If the battery is charging and is full (and has not shown notification yet)
+if [ "$BATTERY_LEVEL" -gt 98 ] && [ "$BATTERY_DISCHARGING" -eq 0 ] && [ ! -f $FULL_FILE ]; then
+	notify-send "Battery Charged" "Battery is fully charged." -i "battery-full" -r 9991
+	touch $FULL_FILE
+# If the battery is low and is not charging (and has not shown notification yet)
+elif [ "$BATTERY_LEVEL" -le $WARNING_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $WARNING_FILE ]; then
+	notify-send "Low Battery" "${BATTERY_LEVEL}% of battery remaining." -u critical -i "battery-warn" -r 9991
+	touch $WARNING_FILE
+# If the battery is critical and is not charging (and has not shown notification yet)
+elif [ "$BATTERY_LEVEL" -le $CRITICAL_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $CRITICAL_FILE ]; then
+	notify-send "Battery Critical" "The computer will shutdown soon." -u critical -i "battery-crit" -r 9991
+	touch $CRITICAL_FILE
+fi
---
+++ scripts/notifications/pop_history.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+dunstctl history-pop
