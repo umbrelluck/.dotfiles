e1f7d83277d5261555b9940d07130fd56dc10dd1
umbrelluck <yarko.umbrelluck@gmail.com>
2024-12-29 00:08:31 +0200

    specific special workplace for pwvucontrol and closing there on right mouse

--- scripts/.scripts/godot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-[ -n "$1" ] && file=$1
-nvim --server ~/.cache/nvim/godot.pipe --remote-send ":e '$file'<CR>"
--- scripts/sound/pwvucontrol.sh
+++ scripts/sound/pwvucontrol.sh
@@ -3,13 +3,17 @@
 # NOTE: for when it is flatpak run
 # proc=$(ps aux | rg 'pwvucontrol' | rg -v 'rg|--|zsh')
 
+# NOTE: for remembering volume level needs to remain active
+
 proc=$( ps aux | rg 'pwvucontrol' | rg -v 'nvim|rg|defunct|zsh')
 if [[ -z "$proc"  ]]; then
     if [[ $IS_UWSM -eq 1 ]]; then
-        uwsm app -- pwvucontrol
+        hyprctl dispatch exec "[workspace special:pwvu] uwsm app -- pwvucontrol"
     else
-        pwvucontrol
+        hyprctl dispatch exec [workspace special:pwvu] pwvucontrol
     fi
+
 else
-    kill $(echo $proc | awk '{print $2}')
+    # kill $(echo $proc | awk '{print $2}')
+    hyprctl dispatch togglespecialworkspace pwvu
 fi
