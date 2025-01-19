a06e3fd9f86af66e8a0749f55c462b3d0f79d1df
umbrelluck <yarko.umbrelluck@gmail.com>
2025-01-19 17:51:01 +0200

    Yazi memfile fix

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -2,4 +2,15 @@ require("full-border"):setup {
     -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
     type = ui.Border.ROUNDED,
 }
+
 require("git"):setup()
+
+require("mime-ext"):setup {
+    with_files = {
+        makefile = "text/makefile",
+    },
+    with_exts = {
+        mk = "text/makefile",
+    },
+    fallback_file1 = true,
+}
