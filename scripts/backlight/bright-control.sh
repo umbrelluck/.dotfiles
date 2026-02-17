140f247949212c4eb0e6f6c6f4c1f8b4df2caac9
umbrelluck <yarko.umbrelluck@gmail.com>
2026-02-18 00:54:06 +0200

    brighness script should work on most intel

--- scripts/backlight/bright-control.sh
+++ scripts/backlight/bright-control.sh
@@ -4,11 +4,16 @@
 [[ $# -lt 1 ]] && echo "Requires flags to work" >&2
 
 
-EDGE=2400 #10%
-CRIT=$((EDGE*5/10)) #5%
+
+# MAX_VALUE=$(brightnessctl -lm  | sed -n '1s/.*,//p;q')
+file="/sys/class/backlight/intel_backlight/max_brightness"
+read -r MAX_VAL < "$file"
+# MAX_VAL="${line##*,}"
+EDGE=$((MAX_VAL/10)) #10%
+CRIT=$((EDGE/2)) #5%
 INC=10
 MIN=2 
-DEVICE_MIN=240 #1%
+DEVICE_MIN=1 #1%
 
 level=$(brightnessctl g)
 
