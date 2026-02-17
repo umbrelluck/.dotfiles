73b199fc71816b6b8cdcc74fbd4bdab0716cba61
umbrelluck <yarko.umbrelluck@gmail.com>
2026-02-17 22:35:21 +0200

    Niri config windowrules

--- scripts/niri/launch_services.sh
+++ scripts/niri/launch_services.sh
@@ -8,9 +8,13 @@ systemctl --user add-wants niri.service walker.service
 systemctl --user add-wants niri.service udiskie.service
 systemctl --user add-wants niri.service copyq.service
 
+systemctl --user add-wants niri.service noctalia.service
+
 systemctl --user add-wants niri.service hypridle.service
 systemctl --user add-wants niri.service hyprlock.service
-systemctl --user add-wants niri.service hyprpaper.service
+# systemctl --user add-wants niri.service hyprpaper.service
+systemctl --user add-wants niri.service waypaper.service
 systemctl --user add-wants niri.service hyprsunset.service
 systemctl --user add-wants niri.service hyprpolkitagent.service
 
+systemctl --user add-wants niri.service wpaperd.service
