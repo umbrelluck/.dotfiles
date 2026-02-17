4a8afd796cae1f46855becd8e676efc714c7aae4
umbrelluck <yarko.umbrelluck@gmail.com>
2026-02-17 22:34:51 +0200

    Mainly updated yai

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,16 +1,22 @@
-require("full-border"):setup {
+require("full-border"):setup({
     -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
     type = ui.Border.ROUNDED,
-}
+})
 
 require("git"):setup()
 
-require("mime-ext"):setup {
+require("mime-ext.local"):setup({
+    -- Expand the existing filename database (lowercase), for example:
     with_files = {
         makefile = "text/makefile",
     },
+
+    -- Expand the existing extension database (lowercase), for example:
     with_exts = {
         mk = "text/makefile",
     },
-    fallback_file1 = true,
-}
+
+    -- If the MIME type is not in both filename and extension databases,
+    -- then fallback to Yazi's preset `mime.local` plugin, which uses `file(1)`
+    fallback_file1 = false,
+})
